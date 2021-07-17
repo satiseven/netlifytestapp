ac715b545c848ed6482cbca796ceebff8c1286d9
satiseven <satiseven777@gmail.com>
2021-07-17 12:35:50 +0300

    search search

--- functions/search.js
+++ functions/search.js
@@ -1,7 +1,9 @@
 const qs = require("querystring");
 const fetch = require("node-fetch");
+
 exports.handler = async (event) => {
   const { query } = qs.parse(event.body);
+
   const response = await fetch(
     `https://api.unsplash.com/search/photos?query=${query}`,
     {
@@ -14,12 +16,20 @@ exports.handler = async (event) => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
+  const firstResult = response.results[0];
+
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
+      "Content-Type": "text/html",
     },
-    body: JSON.stringify({ query }),
     body: JSON.stringify(response),
+    body: `
+       <img
+         src="${firstResult.urls.regular}"
+         alt="${firstResult.alt_description}"
+       />
+     `,
   };
 };
